f94b6982a511bec5aef20059c894aeae998adc60
naderbenmrad <nader.benmrad@gmail.com>
2025-05-14 22:30:42 +0100

    header creation

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import React from 'react';
 import './App.css';
 import Nav from './components/Nav';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
       <Nav/>
+      <Header/>
       <h1>Little Lemon</h1>
     </>
   );
